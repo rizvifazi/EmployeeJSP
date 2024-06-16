dfb49d126b69ab68dafdc7c4eb99e789b61fb13d
Rizvi Fazi <rizvifazi124@gmail.com>
2024-06-16 20:43:05 +0530

    Initialized project

---
+++ src/main/java/com/pack/EmployeeJSP/EmployeeJspApplication.java
@@ -0,0 +1,13 @@
+package com.pack.EmployeeJSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeJspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pack/EmployeeJSP/EmployeeJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pack.EmployeeJSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
